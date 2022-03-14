b6bf034cd76c3840c62b9a72f2e9a75c090135b0
David Sozashvili <davidsozashvili@gmail.com>
2022-03-15 00:50:35 +0400

    create TableHeader

--- src/components/UsersTable/TableContent.js
+++ src/components/UsersTable/TableContent.js
@@ -1,9 +1,54 @@
+import { DropDownIcon, PlusIcon } from "../UI/Icons";
+
+const headings = [
+    {
+        title: 'USER',
+        align: 'left',
+        sortable: true,
+        width: '320px'
+    }, 
+    {
+        title: 'ROLE',
+        align: 'center',
+        sortable: true,
+        width: '100px'
+    }, 
+    {
+        title: 'STATUS',
+        align: 'center',
+        sortable: true,
+        width: '100px'
+    }, 
+    {
+        title: 'ACTIONS',
+        align: 'right',
+        sortable: false,
+        width: '100px'
+    }]
 
 function TableContent() {
-    
-    return (
-        <div>
 
+    return (
+        <div className="mt-20">
+            <div className="border-y-1 border-opacity-10 py-5">
+                <div className="relative basic-container flex justify-between">
+                    <div className="absolute -top-14 -left-3 cursor-pointer">
+                        <PlusIcon />
+                    </div>
+                    {headings.map((heading, i) => (
+                        <div className={`
+                                ${i === 0 ? 'ml-20' : ''}
+                                flex items-center gap-1 text-base font-semibold 
+                                w-${heading.width} text-${heading.align} cursor-pointer`
+                            } 
+                            key={i}
+                        >
+                            <h5>{heading.title}</h5>
+                            {heading.sortable && <DropDownIcon /> }
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/UsersTable/TableHeader.js
+++ src/components/UsersTable/TableHeader.js
@@ -3,8 +3,8 @@ import Input from "../UI/Input";
 function TableHeader() {
 
     return (
-        <div className="container flex justify-between items-center">
-            <h1 className="text-4xl font-titillium font-bold">Project Access</h1>
+        <div className="basic-container flex justify-between items-center">
+            <h1 className="text-4xl font-bold">Project Access</h1>
             <Input type='search' placeholder='Type to filter the table...'/>
         </div>
     );
