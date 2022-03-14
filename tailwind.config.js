987e9fde75b76f551b80d8c145f2cb01d5128b3a
David Sozashvili <davidsozashvili@gmail.com>
2022-03-15 00:51:39 +0400

    confirm tailwind styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,17 +7,10 @@ module.exports = {
       fontFamily: {
         titillium: ['Titillium Web']
       },
-    },
-    container: {
-      center: true,
-      screens: {
-        sm: "100%",
-        md: "100%",
-        lg: "1024px",
-        xl: "1280px",
-        '2xl': "1280px"
-     }
-    },
+      borderWidth: {
+        1: '1px'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
